05120f00c929fd433686f06611b1ff0b1f110474
cander <cander_liubiao@126.com>
2019-08-01 20:27:39 +0800

    先简化

--- cache.go
+++ cache.go
@@ -6,40 +6,16 @@ import (
 	"time"
 )
 
-type auto uint64
 
-func (a auto) Init() {
-
-	go func() {
-		a.reduce()
-	}()
-	select {
-	case a == 0:
-		
-	}
-}
-
-func (a auto) reduce() {
-	t := time.Tick(time.Second)
-	for _ = range t {
-		a = a + 1
-	}
-}
 
 type strvalue struct {
 	key string
 	mu sync.RWMutex
 	value interface{}
-	expiration time.Duration // 过期时间， 单位： 秒
 	start time.Time  // 创建的时间
 }
 
-type element struct {
-	key string
-	ty string  // # key 的类型， 暂时只有str
-}
 
-var el []*element // 保存key, 按照过期时间排序
 
 var gocache *cache
 
@@ -49,16 +25,18 @@ type cache struct {
 }
 
 func Init() {
-	var x auto
-	x.add()
-	el = make([]*element, 0)
 	gocache = &cache{
 		str: make(map[string]*strvalue, 0),
 	}
 }
 
 func Get(key string) interface{} {
-	return gocache.str[key]
+	fmt.Println(time.Since(gocache.str[key].start))
+	if time.Since(gocache.str[key].start) <= 0  {
+		return gocache.str[key].value
+		fmt.Println(time.Since(gocache.str[key].start))
+	}
+	return nil
 }
 
 func Set(key string, value interface{}, d time.Duration) {
@@ -66,22 +44,8 @@ func Set(key string, value interface{}, d time.Duration) {
 		key: key,
 		value: value,
 		mu: sync.RWMutex{},
-		start: time.Now(),
-	}
-	if d > 0 {
-		ss.expiration = d
-		gocache.str[key] = ss
+		start: time.Now().Add(d),
 	}
 	gocache.str[key] = ss
 }
 
-// 按快到期的秒数来排序
-func sort(key string, d uint64) {
-	for _,e := range el {
-		switch  e.ty {
-		case "str":
-			//ep := time.Since(gocache.str[e.key].start.Add(gocache.str[e.key].expiration)).Seconds()
-			print(gocache.str[e.key].expiration)
-		}
-	}
-}
--- doc/main.go
+++ doc/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "cache"
+import (
+	"cache"
+	"fmt"
+	"time"
+)
 
 func main() {
 	cache.Init()
+	cache.Set("aaa", "bbb", time.Second * 10)
+	fmt.Println(cache.Get("aaa"))
+
 }

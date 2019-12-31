863f72039ac6122816fad8f816f4f3b53fa57d82
cander <cander@htdtek.com>
2019-12-31 14:00:24 +0800

    rm

--- cache.go
+++
@@ -1,9 +0,0 @@
-package cachetable
-
-
-type Table interface {
-	Set(key string, value interface{},setkey string,setvalue interface{}) error
- 	Add( interface{}) error
-	Key(key string) error
-	Get(key string, field string, value interface{}) (interface{}, error)
-}
